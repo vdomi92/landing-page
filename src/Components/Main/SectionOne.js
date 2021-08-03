e3b8ae42b55c91f11499e9c7981ef00a0e4959da
Domi <vataidomonkos@gmail.com>
2021-08-03 17:36:58 +0200

    background animation added

--- src/Components/Main/SectionOne.js
+++ src/Components/Main/SectionOne.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import Background from './Images/backgroundImgKitchen.jpg'
+import Background1 from './Images/slideshowbg1.jpg'
+import Background2 from './Images/slideshowbg2.jpg'
+import Background3 from './Images/slideshowbg3.jpg'
 import KitchenProductsImg from './Images/KitchenProducts.jpg'
 import ShowerProductsImg from './Images/ShowerProducts.jpg'
 import { Link } from 'react-router-dom'
@@ -7,15 +9,26 @@ import { Link } from 'react-router-dom'
 export default function SectionOne() {
   return (
     <>
-      <div
-        className='firstDiv'
-        style={{
-          backgroundImage: `url(${Background})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-        }}
-      ></div>
+      <div className='slideShowDiv'>
+        <div
+          className='slideShowFirst slideElement'
+          style={{
+            backgroundImage: `url(${Background1})`,
+          }}
+        ></div>
+        <div
+          className='slideShowSecond slideElement'
+          style={{
+            backgroundImage: `url(${Background2})`,
+          }}
+        ></div>
+        <div
+          className='slideShowThird slideElement'
+          style={{
+            backgroundImage: `url(${Background3})`,
+          }}
+        ></div>
+      </div>
       <div className='mt-5 productLinkDiv productContainer'>
         <h1 className='text-center headingOne'>DiLo. A víz a mindenünk.</h1>
         <div className='row prodLinkContainer'>
